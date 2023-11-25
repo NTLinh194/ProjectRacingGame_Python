2ac298480e7ba000cbe2ce548200fe4bffc3ba47
triwjbu <minhtriqt04@gmail.com>
2023-11-25 09:37:00 +0700

    thêm bảng high score

--- button.py
+++ button.py
@@ -2,6 +2,7 @@ import pygame
 
 #button class
 class Button():
+	mouse_holding=False
 	def __init__(self, x, y, image, scale):
 		width = image.get_width()
 		height = image.get_height()
@@ -12,18 +13,16 @@ class Button():
 
 	def draw(self, surface):
 		action = False
-		#lấy vị trí của chuột
-		pos = pygame.mouse.get_pos()
-
 		#kiểm tra xem chuột đang có hover trên button và đang click hay không
-		if self.rect.collidepoint(pos):
-			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-				self.clicked = True
+		if self.rect.collidepoint(pygame.mouse.get_pos()):
+			if  pygame.mouse.get_pressed()[0] == 1 and self.clicked == False and self.__class__.mouse_holding == False :
+				self.clicked = True	
+				self.__class__.mouse_holding=True
+			if  pygame.mouse.get_pressed()[0] == 0 and self.clicked == True :
 				action = True
-
 		if pygame.mouse.get_pressed()[0] == 0:
 			self.clicked = False
-
+			self.__class__.mouse_holding=False
 		#hiển thị button
 		surface.blit(self.image, (self.rect.x, self.rect.y))
 
